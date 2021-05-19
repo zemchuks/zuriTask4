a2f550710ef4dad13c30fdf7817ff841b10e9be6
Bruno Austin <brunoaugustine2@gmail.com>
2021-05-19 17:17:31 +0100

    add readme file

---
+++ READme.md
@@ -0,0 +1,4 @@
+Zuri Task 4 - Algorithms 
+---
+
+Click [Here](https://zemchuks.github.io/zuriTask4/) to view  the application Demo
--- checkYuGiOh.js
+++ checkYuGiOh.js
@@ -1,9 +1,44 @@
-function checkYuGiOh(n) {
-    let checkArray = [1, "yu", "gi", "oh"]
-    let arrWithNums = [];
-    for (i = 1; i < n; i++) {
-     arrWithNums.push(Math.floor(Math.random() * 10));
+const checkYuGiOh = function (n) {
+
+    //creates empty array
+  let arrayval = [];
+
+  //checks if the argument is a number
+  if (typeof n != 'number') {
+    console.log(`invalid paremeter: ${n}`);
+  } 
+  //if its a number then
+  else if (typeof n == 'number') {
+      //starts at 1 loops through n (number we passed in) and adds it to the arrayval
+    for (let i = 1; i <= n; i++) {
+        //pushes the value at i to the array
+      arrayval.push(i);
+
+      //loops through our created array
+      for (let j = 0; j < arrayval.length; j++) {
+        if (arrayval[j] % 2 == 0 && arrayval[j] % 3 == 0) {
+          arrayval[j] = 'yu-gi';
+        }
+         else if (arrayval[j] % 2 == 0 && arrayval[j] % 5 == 0) {
+          arrayval[j] = 'yu-oh';
+        }
+         else if ( arrayval[j] % 2 == 0 && arrayval[j] % 3 == 0 &&
+          arrayval[j] % 5 == 0
+        ) {
+          arrayval[j] = 'yu-ji-oh';
+        } else if (arrayval[j] % 2 == 0) {
+          arrayval[j] = 'yu';
+        } else if (arrayval[j] % 3 == 0) {
+          arrayval[j] = 'gi';
+        } else if (arrayval[j] % 5 == 0) {
+          arrayval[j] = 'oh';
+        }
+      }
     }
-    return arrWithNums;
-} 
-console.log(checkYuGiOh(10))
+  }
+  return arrayval;
+};
+
+console.log(checkYuGiOh(10));
+console.log(checkYuGiOh(5))
+console.log(checkYuGiOh("fizzbuzz is meh"))
